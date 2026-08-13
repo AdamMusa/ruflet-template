0feafb218faa8181de5def62fd1fcd712e6dd9b6
AdamMusa <adammusa2222@gmail.com>
2026-08-13 04:28:39 -0400

    Remove legacy Apple renderer implementation

--- templates/ruflet_flutter_template/apple_packages/ruflet_apple/Sources/RufletFlashlight/RufletFlashlight.swift
+++
@@ -1,9 +0,0 @@
-import RufletEngine
-
-/// Native Apple implementation of Flet's `flet_flashlight` package.
-@MainActor
-public enum RufletFlashlight: RufletExtension {
-  public static func register(in registry: ServiceRegistry) {
-    registry.registerNamed("Flashlight") { FlashlightService() }
-  }
-}
--- templates/ruflet_flutter_template/apple_packages/ruflet_apple/Sources/RufletPermissionHandler/RufletPermissionHandler.swift
+++
@@ -1,9 +0,0 @@
-import RufletEngine
-
-/// Native Apple implementation of Flet's `flet_permission_handler` package.
-@MainActor
-public enum RufletPermissionHandler: RufletExtension {
-  public static func register(in registry: ServiceRegistry) {
-    registry.register(PermissionHandlerService.self) { PermissionHandlerService() }
-  }
-}
